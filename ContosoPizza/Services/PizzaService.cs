22d2383cbf5fb359f70099ee06c54d3aabb8ebcd
talitapereira <talitarsp@outlook.com>
2024-09-06 18:54:23 -0300

    adicionando banco de dados e outras classes

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -1,41 +1,19 @@
 using ContosoPizza.Models;
+using ContosoPizza.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoPizza.Services;
 
 public class PizzaService
 {
-    public PizzaService()
+    private readonly PizzaContext _context;
+
+    public PizzaService(PizzaContext context)
     {
-        
+        _context = context;
     }
 
-    public IEnumerable<Pizza> GetAll()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Pizza? GetById(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Pizza? Create(Pizza newPizza)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void AddTopping(int PizzaId, int ToppingId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void UpdateSauce(int PizzaId, int SauceId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteById(int id)
-    {
-        throw new NotImplementedException();
-    }
+    /// ...
+    /// CRUD operations removed for brevity
+    /// ...
 }
